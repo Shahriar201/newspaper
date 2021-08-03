313fc18efb7dae75830c36c685dad243e3c616bf
shahriar201 <shahriarbrine201@gmail.com>
2021-08-03 20:07:40 +0600

    Middleware setup

--- routes/web.php
+++ routes/web.php
@@ -13,29 +13,32 @@ Route::get('login/{website}/callback', 'Auth\LoginController@handleProviderCallb
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::prefix('users')->group(function(){
-    Route::get('/view', 'Backend\UserController@view')->name('users.view');
-    Route::get('/add', 'Backend\UserController@add')->name('users.add');
-    Route::post('/store', 'Backend\UserController@store')->name('users.store');  
-    Route::get('/edit/{id}', 'Backend\UserController@edit')->name('users.edit');
-    Route::post('/update/{id}', 'Backend\UserController@update')->name('users.update');
-    Route::post('/delete', 'Backend\UserController@delete')->name('users.delete');      
-});
-
-Route::prefix('profiles')->group(function(){   
-    Route::get('/view', 'Backend\ProfileController@view')->name('profiles.view');
-    Route::get('/edit', 'Backend\ProfileController@edit')->name('profiles.edit');
-    Route::post('/store', 'Backend\ProfileController@update')->name('profiles.update');
-    Route::get('/password/view', 'Backend\ProfileController@passwordView')->name('profiles.password.view');
-    Route::post('/password/update', 'Backend\ProfileController@passwordUpdate')->name('profiles.password.update');
+Route::group(['middleware'=>'admin'], function(){
+    Route::prefix('users')->group(function(){
+        Route::get('/view', 'Backend\UserController@view')->name('users.view');
+        Route::get('/add', 'Backend\UserController@add')->name('users.add');
+        Route::post('/store', 'Backend\UserController@store')->name('users.store');  
+        Route::get('/edit/{id}', 'Backend\UserController@edit')->name('users.edit');
+        Route::post('/update/{id}', 'Backend\UserController@update')->name('users.update');
+        Route::post('/delete', 'Backend\UserController@delete')->name('users.delete');      
+    });
     
+    Route::prefix('profiles')->group(function(){   
+        Route::get('/view', 'Backend\ProfileController@view')->name('profiles.view');
+        Route::get('/edit', 'Backend\ProfileController@edit')->name('profiles.edit');
+        Route::post('/store', 'Backend\ProfileController@update')->name('profiles.update');
+        Route::get('/password/view', 'Backend\ProfileController@passwordView')->name('profiles.password.view');
+        Route::post('/password/update', 'Backend\ProfileController@passwordUpdate')->name('profiles.password.update');
+        
+    });
+    
+    Route::prefix('categories')->group(function(){
+        Route::get('/view', 'Backend\CategoryController@view')->name('categories.view');
+        Route::get('/add', 'Backend\CategoryController@add')->name('categories.add');
+        Route::post('/store', 'Backend\CategoryController@store')->name('categories.store');  
+        Route::get('/edit/{id}', 'Backend\CategoryController@edit')->name('categories.edit');
+        Route::post('/update/{id}', 'Backend\CategoryController@update')->name('categories.update');
+        Route::post('/delete', 'Backend\CategoryController@delete')->name('categories.delete');      
+    });
 });
 
-Route::prefix('categories')->group(function(){
-    Route::get('/view', 'Backend\CategoryController@view')->name('categories.view');
-    Route::get('/add', 'Backend\CategoryController@add')->name('categories.add');
-    Route::post('/store', 'Backend\CategoryController@store')->name('categories.store');  
-    Route::get('/edit/{id}', 'Backend\CategoryController@edit')->name('categories.edit');
-    Route::post('/update/{id}', 'Backend\CategoryController@update')->name('categories.update');
-    Route::post('/delete', 'Backend\CategoryController@delete')->name('categories.delete');      
-});
